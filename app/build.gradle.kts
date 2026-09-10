7c6e6ce995102710d64cde1014bcb5d0ccdfefbb
siddiquenazib2005-dot <siddiquenazib2005@gmail.com>
2026-09-10 10:07:28 +0530

    Update from GitHub Zip Pusher: app/build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -23,8 +23,8 @@ android {
         applicationId = "com.aurix.ai"
         minSdk = 26
         targetSdk = 35
-        versionCode = 4
-        versionName = "1.3"
+        versionCode = 6
+        versionName = "1.5"
         vectorDrawables {
             useSupportLibrary = true
         }
